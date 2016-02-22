7dac87c1d347122dffb00617d16f2f806a625541
Pedro Henrique Rodrigues <pedrohenriquerls@gmail.com>
2016-02-21 22:52:52 -0300

    mocks for twitter_crawler specs

--- twitter_crawler/spec/lib/twitter_client_spec.rb
+++ twitter_crawler/spec/lib/twitter_client_spec.rb
@@ -1,8 +1,26 @@
-require 'rspec'
+require 'spec_helper'
 require_relative '../../lib/twitter_client'
 
+class FakeConnection
+  def initialize(body)
+    @body = body
+  end
+
+  def stream(_, response)
+    @body.each_line do |line|
+      response.on_body(line)
+    end
+  end
+end
+
+
 describe TwitterClient do
+  before(:each) do
+    TwitterClient.instance.client.connection = FakeConnection.new(fixture('track_streaming.json'))
+    stub_request(:any, /localhost:3000/).to_return(:status => [200, 'All tweets are saved with success'])
+  end
+
   it '' do
-    TwitterClient.instance
+    TwitterClient.instance.track_tweets 'code'
   end
 end
---
+++ twitter_crawler/spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'twitter'
+require 'rspec'
+require 'webmock/rspec'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
+def fixture_path
+  File.expand_path('../fixtures', __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end
